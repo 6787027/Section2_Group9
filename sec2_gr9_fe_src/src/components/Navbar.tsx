5d48741346b4d716ce362524e5b2b8f8f87f13df
thananchanok51248 <thananchanok.chu@student.mahidol.edu>
2025-10-17 02:11:28 +0700

    nav-1

--- sec2_gr9_fe_src/src/components/Navbar.tsx
+++ sec2_gr9_fe_src/src/components/Navbar.tsx
@@ -1,16 +1,38 @@
 "use client";
+import Image from "next/image";
 import Link from "next/link";
+import Logo from '../assets/logo-navbar.svg'
+import { faSearch,faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Navbar() {
   return (
-    <div>
+    <div >
       <main>
-        <div className="navbar bg-gray-100 w-full h-10 text-white px-8 py-2 flex justify-between">
-          
-        </div>
-        <div>
+        <div className="bg-white w-full h-10 text-white  px-6 flex justify-between border border-b-gray-200 z-30">
+
+          <Image alt="logo-nav" src={Logo} width='180' className="float-left"/>
+
+          <div className="flex justify-items-end bg-white rounded-4xl  mr-10  w-80  border-2 border-gray-200 my-1">
+            <input 
+              className="text-black rounded-4xl pl-2 w-68 pl-8 outline-none"
+              placeholder="Search"
+            ></input>
+            <a href="/home"><FontAwesomeIcon icon={faSearch} className="text-black ml-2"/>  </a>
+          </div>
+
+
+          <div>
+             <a href="https://www.pinterest.com/"> <FontAwesomeIcon icon={faUser} size="xl" className="text-black " /> </a>
+          </div>
 
         </div>
+
+        <div className="bg-white w-full h-10 z-20 shadow-xl" >
+          fesfesffesfesfs
+        </div>
+        
+
       </main>
 
     </div>
