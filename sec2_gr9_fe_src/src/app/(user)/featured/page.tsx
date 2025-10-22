5ef1ceb23d6d518281c7beb7089bc600628586ae
thita034 <thita2710@gmail.com>
2025-10-22 17:00:02 +0700

    Feature Page

--- sec2_gr9_fe_src/src/app/(user)/featured/page.tsx
+++ sec2_gr9_fe_src/src/app/(user)/featured/page.tsx
@@ -15,18 +15,19 @@ export default function Featured() {
           {/* image1 */}
           <div className="min-h-screen w-screen mt-8 content-end" style={{ backgroundImage: `url(${image1.src})` }}>
             <div className="pb-20">
-              <Link href={"/product"}><button className="btn hover:bg-[#A599ED] active:bg-black rounded-xl border-0">Show now</button> </Link>
+              <Link href={"/product"}>
+                <button className="text-lg h-14 w-40 text-[#975b0d] font-semibold bg-white/50 backdrop-blur-sm hover:bg-[#975b0d] hover:text-white active:bg-white rounded-4xl border-0 transition-all delay-200">Show now</button>
+              </Link>
             </div>
           </div>
 
           {/* image2 */}
-          <div className="min-h-screen w-screen flex justify-items-start items-end " style={{ backgroundImage: `url(${image2.src})` }}>
+          <div className="min-h-screen w-screen flex justify-items-start items-end" style={{ backgroundImage: `url(${image2.src})` }}>
             <div className="p-40">
-              <Link href={"/product"}><button className="btn hover:bg-[#A599ED] active:bg-black rounded-xl border-0">Show now</button> </Link>
+              <Link href={"/product"}><button className="text-lg h-14 w-40 text-[#6759b9] font-semibold bg-white/50 backdrop-blur-sm hover:bg-[#6759b9] hover:text-white active:bg-white rounded-4xl border-0 transition-all delay-200">Show now</button> </Link>
             </div>
           </div>
         </div>
-
       </main>
 
     </div>
