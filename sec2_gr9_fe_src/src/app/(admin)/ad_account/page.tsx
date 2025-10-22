496b2010b8841f1ca9968327a05451d8459b4b8d
YEONOIR <matsukos128@gmail.com>
2025-10-22 19:59:16 +0700

    nongnoeyjaa

---
+++ sec2_gr9_fe_src/src/app/(admin)/ad_account/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Ad_account() {
+  return (
+   <div className="flex flex-col items-center justify-center min-h-screen p-8 text-center">
+      <h1 className="text-3xl font-bold mb-4">Admin</h1>
+      <p className="text-lg max-w-2xl text-gray-700 dark:text-gray-300">
+        Admin using Next.js and Tailwind CSS.
+      </p>
+    </div>
+  );
+}
--- sec2_gr9_fe_src/src/app/layout.tsx
+++ sec2_gr9_fe_src/src/app/layout.tsx
@@ -1,9 +1,8 @@
-import Navbar from "../../components/Navbar";
-import Footer from "@/components/footer";
+import "@/app/globals.css";
 
 export const metadata = { title: "CelesteCrat", description: "Next.js App" };
 
-export default function UserLayout({ children }: { children: React.ReactNode }) {
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
@@ -12,11 +11,8 @@ export default function UserLayout({ children }: { children: React.ReactNode }) 
     <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet" />
     </head>
       <body className="min-h-screen ">
-        <Navbar />
         
         {children}
-
-        <Footer/>
 
       </body>
        
