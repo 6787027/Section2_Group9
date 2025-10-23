0802bae5947cd56954800585197d518e6c94763a
premmy <nattanon.nga@student.mahidol.ac.th>
2025-10-23 15:20:30 +0700

    Cart 1st-update

--- sec2_gr9_fe_src/src/app/(user)/cart/page.tsx
+++ sec2_gr9_fe_src/src/app/(user)/cart/page.tsx
@@ -1,11 +1,30 @@
-
+import { AuroraText } from "@/components/ui/aurora-text"
+import Image from "next/image";
+import ImgOrder from "@/assets/ImageOrder.png"
 export default function Cart() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 text-center">
-      <h1 className="text-3xl font-bold mb-4">My Cart</h1>
-      <p className="text-lg max-w-2xl text-gray-700 dark:text-gray-300">
-        Welcome to our website. We build modern web applications using Next.js and Tailwind CSS.
-      </p>
+    <div className="flex-col flex items-center min-h-screen p-8 text-ce bg-[#282151]">
+      <div className="bg-white flex flex-1 w-full rounded-4xl">
+        <div className="pl-10 ">
+          <h1 className="pt-10 text-[#240046] text-3xl font-bold">MY CART</h1>
+          <div className="pl-20 pt-10">
+            <div className="flex gap-40 ">
+              <h3>PRODUCT</h3>
+              <h3>PRICE</h3>
+              <h3>QTY</h3>
+              <h3>TOTAL</h3>
+            </div>
+          </div>
+          <hr className="my-4 border-t-2 border-gray-300 w-auto" />
+          <div className="flex">
+            <Image src={ImgOrder} alt="ProductImg" />
+          </div>
+          
+        </div>
+      </div> 
+      
+      
+      
     </div>
   );
 }
