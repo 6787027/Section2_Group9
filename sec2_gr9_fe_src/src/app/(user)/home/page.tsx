e95b556e217c62fec7e3afb08b3b0fab0ad1194b
thananchanok51248 <thananchanok.chu@student.mahidol.edu>
2025-10-24 11:02:44 +0700

    home not home

--- sec2_gr9_fe_src/src/app/(user)/home/page.tsx
+++ sec2_gr9_fe_src/src/app/(user)/home/page.tsx
@@ -1,9 +1,21 @@
-
+import Link from "next/link";
+import { LogOut } from 'lucide-react';
 
 export default function Home() {
   return (
-   <div className="bg-[#282151] flex flex-col items-center justify-center min-h-screen p-8 text-center">
+   <div className="bg-[#282151]  min-h-screen  text-center">
       <main>
+        logout
+        <div className="  justify-items-end items-baseline-last  px-4">
+          <div>
+            
+          </div>
+          <div className="flex flex-row">
+            <Link href={""}><button className=" bg-white  px-4 py-2 rounded-lg shadow-xl hover:bg-[#948AD2] flex flex-row " ><LogOut className="mr-2"></LogOut> logout</button> </Link>
+          </div>
+          </div>
+        
+        <div className=" flex flex-col items-center justify-center pt-12">
         <div className="bg-linear-to-t from-[#948AD2]  to-[#FFE6E6] rounded-lg p-10">
 
           {/* header */}
@@ -18,7 +30,7 @@ export default function Home() {
                 <span className=" text-sm">First name</span>
               </div>
               {/* box */}
-              <div className="bg-white py-2 px-4 rounded-xl grid justify-items-start">
+              <div className="bg-white py-2 px-4  shadow-xl rounded-xl grid justify-items-start">
                 <span>Thananchanok </span>
               </div>
             </div>
@@ -29,7 +41,7 @@ export default function Home() {
                 <span className=" text-sm">First name</span>
               </div>
               {/* box */}
-              <div className="bg-white py-2 px-4 rounded-xl grid justify-items-start ">
+              <div className="bg-white py-2 px-4 shadow-xl rounded-xl grid justify-items-start ">
                 <span>Chuensang</span>
               </div>
               </div>
@@ -43,13 +55,33 @@ export default function Home() {
               <span className=" text-sm">Email</span>
             </div>
 
-            <div className="bg-white py-2 px-4 rounded-xl  grid justify-items-start">
+            <div className="bg-white py-2 px-4 rounded-xl  shadow-xl grid justify-items-start">
               <span>thananchanok.chu@student.mahidol.edu</span>
             </div>
           </div>
 
+          {/* phonenumber */}
+          <div>
+          <div className="grid justify-items-start py-2">
+            <span>PhoneNUmber</span>
+          </div>
 
+          <div className="bg-white py-2 px-4 rounded-xl  shadow-xl grid justify-items-start">
+            <span>0935299979</span>
+          </div>
+          </div>         
 
+          {/* edit profile  */}
+          <div className="py-8">
+            <div className="" >
+              <Link href={""}><button className=" bg-[#C8C4EE] px-48 py-2 rounded-xl shadow-xl hover:bg-[#ACA5EC]">Edit Profile</button> </Link>
+            </div>
+            <div className="pt-4">
+               <Link href={""}><button className=" bg-[#FFE6E6] px-48 py-2 rounded-xl shadow-xl hover:bg-[#FFB2B2]">View Order</button> </Link>
+            </div>
+          </div>
+
+        </div>
         </div>
       </main>
     </div>
