e58936d8b14806805d02753621be6073653a2f1e
thita034 <thita2710@gmail.com>
2025-11-05 10:42:14 +0700

    feature image change

--- sec2_gr9_fe_src/src/app/(user)/featured/page.tsx
+++ sec2_gr9_fe_src/src/app/(user)/featured/page.tsx
@@ -13,7 +13,7 @@ export default function Featured() {
             <AuroraText className="text-6xl font-bold" colors={["#FFDFEF", "#EABDE6", "#D69ADE", "#AA60C8"]}>FEATURED</AuroraText>
           </div>
           {/* image1 */}
-          <div className="min-h-screen w-screen mt-8 content-end" style={{ backgroundImage: `url(${image1.src})` }}>
+          <div className="min-h-screen w-screen mt-8 content-end bg-cover bg-center" style={{ backgroundImage: `url(${image1.src})` }}>
             <div className="pb-20">
               <Link href={"/product"}>
                 <button className="text-lg h-14 w-40 text-[#975b0d] font-semibold bg-white/50 backdrop-blur-sm hover:bg-[#975b0d] hover:text-white active:bg-white rounded-4xl border-0 transition-all delay-200">Shop now</button>
@@ -22,7 +22,7 @@ export default function Featured() {
           </div>
 
           {/* image2 */}
-          <div className="min-h-screen w-screen flex justify-items-start items-end" style={{ backgroundImage: `url(${image2.src})` }}>
+          <div className="min-h-screen w-screen flex justify-items-start items-end bg-cover bg-center" style={{ backgroundImage: `url(${image2.src})` }}>
             <div className="p-40">
               <Link href={"/product"}><button className="text-lg h-14 w-40 text-[#6759b9] font-semibold bg-white/50 backdrop-blur-sm hover:bg-[#6759b9] hover:text-white active:bg-white rounded-4xl border-0 transition-all delay-200">Shop now</button> </Link>
             </div>
