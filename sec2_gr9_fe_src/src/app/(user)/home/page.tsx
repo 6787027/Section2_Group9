67b7086eb9e7c686be0a50f73755958e244fc6c2
Thananchanok Chuensang <>
2025-11-05 10:17:48 +0700

    homeeee

--- sec2_gr9_fe_src/src/app/(user)/home/page.tsx
+++ sec2_gr9_fe_src/src/app/(user)/home/page.tsx
@@ -1,73 +1,82 @@
 import Link from "next/link";
 import Image from "next/image";
 import { AuroraText } from "@/components/ui/aurora-text"
+import home from "@/assets/Home.svg"
 
 
 export default function Home() {
   return (
-    <div className="bg-[#282151]  min-h-screen  text-center px-4">
+    <div className="bg-[#282151]  min-h-screen  text-center ">
       <main>
         {/* Ad  */}
-        <div>
+        <div className=" min-h-screen relative w-full h-screen">
+
+          <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url(${home.src})` }} > </div>
 
         </div>
-
-        {/* product  */}
+        {/* text  */}
         <div>
-          
-          <div><AuroraText className="text-4xl font-bold " colors={["#FFDFEF", "#EABDE6", "#D69ADE", "#AA60C8"]} speed={2} >Recommended Products</AuroraText></div>
-          
+          <div className="flex flex-col pt-12 pb-24"> 
+            <span className="text-3xl text-white font-semibold ">"Welcome to an enchanting world,</span>
+            <span className="text-3xl text-white font-semibold "> brimming with magical energy that is ready to assist you."</span>
+          </div>
+        </div>
+        {/* product  */}
+        <div className="py-8">
+
+          <div className="py-8"><AuroraText className="text-4xl font-bold  " colors={["#FFDFEF", "#EABDE6", "#D69ADE", "#AA60C8"]} speed={2} >Recommended Products</AuroraText></div>
+
           <div className="flex overflow-x-auto  py-4 mx-10 flex-row gap-10 item-center justify-center">
 
             <div className="bg-white p-4 rounded-2xl no-scrollbar ">
               {/* image  */}
               <div>
-                <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover"/>
+                <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover" />
               </div>
               { /* text  */}
               <div className="text-left flex flex-col pt-2 text-7469B6 text-lg">
                 <span className="font-semibold">Fortuna Doll</span>
                 <span>฿ 590</span>
-              </div> 
+              </div>
             </div>
 
             <div className="bg-white p-4 rounded-2xl no-scrollbar ">
               {/* image  */}
               <div>
-                <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover"/>
+                <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover" />
               </div>
               { /* text  */}
               <div className="text-left flex flex-col pt-2 text-7469B6 text-lg">
                 <span className="font-semibold">Fortuna Doll</span>
                 <span>฿ 590</span>
-              </div> 
+              </div>
             </div>
 
             <div className="bg-white p-4 rounded-2xl no-scrollbar ">
               {/* image  */}
               <div>
-                <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover"/>
+                <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover" />
               </div>
               { /* text  */}
               <div className="text-left flex flex-col pt-2 text-7469B6 text-lg">
                 <span className="font-semibold">Fortuna Doll</span>
                 <span>฿ 590</span>
-              </div> 
+              </div>
             </div>
 
             <div className="bg-white p-4 rounded-2xl no-scrollbar ">
               {/* image  */}
               <div>
-                <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover"/>
+                <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover" />
               </div>
               { /* text  */}
               <div className="text-left flex flex-col pt-2 text-7469B6 text-lg">
                 <span className="font-semibold">Fortuna Doll</span>
                 <span>฿ 590</span>
-              </div> 
+              </div>
             </div>
-            
-            
+
+
 
 
           </div>
