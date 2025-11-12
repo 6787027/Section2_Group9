e64c6c5541040c8e13e956c8a8c9a10af050b6c0
Nattanon Ngamkham <Nattanon.nga@student.mahidol.ac.th>
2025-11-12 10:06:56 +0700

    Prem External API home

--- sec2_gr9_fe_src/src/app/(user)/home/page.tsx
+++ sec2_gr9_fe_src/src/app/(user)/home/page.tsx
@@ -1,97 +1,125 @@
+'use client';
+import { useState } from 'react';
 import Link from "next/link";
 import Image from "next/image";
-import { AuroraText } from "@/components/ui/aurora-text"
-import home from "@/assets/Home.svg"
+import { AuroraText } from "@/components/ui/aurora-text";
+import home from "@/assets/Home.svg";
+import TarotModal from '@/components/built-components/tarotModal';
 
+export type TarotCard = {
+  name: string;
+  name_short: string;
+  meaning_up: string;
+  desc: string;
+};
 
 export default function Home() {
-  
+  const [card, setCard] = useState<TarotCard | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleRandomCard = async () => {
+    setIsLoading(true);
+    setError(null);
+    setCard(null);
+
+    try {
+      const response = await fetch("https://tarotapi.dev/api/v1/cards/random?n=1");
+      if (!response.ok) {
+        throw new Error("Failed to fetch card from API.");
+      }
+      const data = await response.json();
+      
+      setCard(data.cards[0]);
+      setIsModalOpen(true);
+    } catch (err) {
+      setError(err instanceof Error ? err.message : "An unknown error occurred.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div className="bg-[#282151]  min-h-screen  text-center ">
-      <main>
-        {/* Ad  */}
-        <div className=" min-h-screen relative w-full h-screen">
-
-          <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url(${home.src})` }} > </div>
-
-        </div>
-        {/* text  */}
-        <div>
-          <div className="flex flex-col pt-12 pb-24"> 
-            <span className="text-3xl text-white font-semibold ">"Welcome to an enchanting world,</span>
-            <span className="text-3xl text-white font-semibold "> brimming with magical energy that is ready to assist you."</span>
-          </div>
-        </div>
-        {/* product  */}
-        <div className="py-8">
-
-          <div className="py-8"><AuroraText className="text-4xl font-bold  " colors={["#FFDFEF", "#EABDE6", "#D69ADE", "#AA60C8"]} speed={2} >Recommended Products</AuroraText></div>
-
-          <div className="flex overflow-x-auto  py-4 mx-10 flex-row gap-10 item-center justify-center">
-
-            <div className="bg-white p-4 rounded-2xl no-scrollbar ">
-              {/* image  */}
-              <div>
-                <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover" />
-              </div>
-              { /* text  */}
-              <div className="text-left flex flex-col pt-2 text-7469B6 text-lg">
-                <span className="font-semibold">Fortuna Doll</span>
-                <span>฿ 590</span>
-              </div>
-            </div>
-
-            <div className="bg-white p-4 rounded-2xl no-scrollbar ">
-              {/* image  */}
-              <div>
-                <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover" />
-              </div>
-              { /* text  */}
-              <div className="text-left flex flex-col pt-2 text-7469B6 text-lg">
-                <span className="font-semibold">Fortuna Doll</span>
-                <span>฿ 590</span>
-              </div>
-            </div>
-
-            <div className="bg-white p-4 rounded-2xl no-scrollbar ">
-              {/* image  */}
-              <div>
-                <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover" />
-              </div>
-              { /* text  */}
-              <div className="text-left flex flex-col pt-2 text-7469B6 text-lg">
-                <span className="font-semibold">Fortuna Doll</span>
-                <span>฿ 590</span>
-              </div>
-            </div>
-
-            <div className="bg-white p-4 rounded-2xl no-scrollbar ">
-              {/* image  */}
-              <div>
-                <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover" />
-              </div>
-              { /* text  */}
-              <div className="text-left flex flex-col pt-2 text-7469B6 text-lg">
-                <span className="font-semibold">Fortuna Doll</span>
-                <span>฿ 590</span>
-              </div>
-            </div>
-
-
-
-
+    <>
+      <div className="bg-[#282151] min-h-screen text-center ">
+        <main>
+          <div className=" min-h-screen relative w-full h-screen">
+            <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url(${home.src})` }} > </div>
           </div>
 
+          <div>
+            <div className="flex flex-col pt-12 pb-12">
+              <span className="text-3xl text-white font-semibold ">"Welcome to an enchanting world,</span>
+              <span className="text-3xl text-white font-semibold "> brimming with magical energy that is ready to assist you."</span>
+            </div>
+          </div>
 
-        </div>
+          <div className="py-12">
+            <button
+              onClick={handleRandomCard}
+              disabled={isLoading}
+              className="bg-gradient-to-r from-purple-500 to-indigo-600 text-white font-bold py-3 px-8 rounded-full shadow-lg transform transition-all duration-300 hover:scale-105 disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              {isLoading ? "Load your future" : "Pick a Card"}
+            </button>
+            {error && <p className="text-red-400 mt-4">{error}</p>}
+          </div>
 
+          <div className="py-8">
+            <div className="py-8"><AuroraText className="text-4xl font-bold" colors={["#FFDFEF", "#EABDE6", "#D69ADE", "#AA60C8"]} speed={2} >Recommended Products</AuroraText></div>
+            <div className="flex overflow-x-auto py-4 mx-10 flex-row gap-10 item-center justify-center">
+              <div className="bg-white p-4 rounded-2xl no-scrollbar ">
+                <div>
+                  <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover" />
+                </div>
+                <div className="text-left flex flex-col pt-2 text-7469B6 text-lg">
+                  <span className="font-semibold">Fortuna Doll</span>
+                  <span>฿ 590</span>
+                </div>
+              </div>
 
-        {/* Celeste Craft  */}
-        <div>
+              <div className="bg-white p-4 rounded-2xl no-scrollbar ">
+                <div>
+                  <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover" />
+                </div>
+                <div className="text-left flex flex-col pt-2 text-7469B6 text-lg">
+                  <span className="font-semibold">Fortuna Doll</span>
+                  <span>฿ 590</span>
+                </div>
+              </div>
 
-        </div>
+              <div className="bg-white p-4 rounded-2xl no-scrollbar ">
+                <div>
+                  <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover" />
+                </div>
+                <div className="text-left flex flex-col pt-2 text-7469B6 text-lg">
+                  <span className="font-semibold">Fortuna Doll</span>
+                  <span>฿ 590</span>
+                </div>
+              </div>
 
-      </main>
-    </div>
+              <div className="bg-white p-4 rounded-2xl no-scrollbar ">
+                <div>
+                  <Image alt="logo" src={"https://i.pinimg.com/736x/eb/63/dd/eb63ddb0fb4c04a9c7bad03671130f18.jpg"} width={250} height={360} className=" rounded-2xl w-full h-full object-cover" />
+                </div>
+                <div className="text-left flex flex-col pt-2 text-7469B6 text-lg">
+                  <span className="font-semibold">Fortuna Doll</span>
+                  <span>฿ 590</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+          </div>
+        </main>
+      </div>
+
+      <TarotModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        card={card}
+      />
+    </>
   );
 }
---
+++ sec2_gr9_fe_src/src/components/built-components/tarotModal.tsx
@@ -0,0 +1,46 @@
+'use client';
+import Image from 'next/image';
+import { TarotCard } from '@/app/(user)/home/page';
+
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  card: TarotCard | null;
+};
+
+
+export default function TarotModal({ isOpen, onClose, card }: ModalProps) {
+  if (!isOpen || !card) return null;
+
+
+  return (
+    <div
+      className="fixed inset-0 bg-[rgba(0,0,0,0.7)] backdrop-blur-md z-50 flex justify-center items-center p-4"
+      onClick={onClose}
+    >
+      <div
+        className="bg-white rounded-2xl p-6 max-w-2xl w-full h- text-black text-left relative shadow-xl"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-3 text-gray-500 hover:text-gray-800 text-3xl font-bold"
+        >
+          &times;
+        </button>
+
+        <div className="mt-4">
+          <h3 className="text-2xl font-bold text-center text-[#282151]">
+            {card.name}
+          </h3>
+          <p className="text-md text-gray-700 mt-2 text-center">
+            <strong>Meaning:</strong> {card.meaning_up}
+          </p>
+          <p className="text-sm text-gray-500 mt-4">
+            {card.desc}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
