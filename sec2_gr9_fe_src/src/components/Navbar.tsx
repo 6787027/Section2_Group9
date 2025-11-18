4f0493740573c3c6208cec78721bc0389372a38c
Thananchanok Chuensang <thananchanok.chu@student.mahidol.edu>
2025-11-19 03:45:14 +0700

    minor change

--- sec2_gr9_fe_src/src/components/Navbar.tsx
+++ sec2_gr9_fe_src/src/components/Navbar.tsx
@@ -52,9 +52,5 @@ export default function Navbar() {
       </main>
 
     </div>
-    // <nav className="w-full flex justify-center gap-8 p-4 bg-gray-100 dark:bg-gray-900">
-    //   <Link href="/home" className="text-blue-600 hover:underline">Home</Link>
-    //   <Link href="/aboutus" className="text-blue-600 hover:underline">About Us</Link>
-    // </nav>
   );
 }
